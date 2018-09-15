399e6506561b466285bb67b07db2b6c920fc4d9a
bill <collinsw@gmail.com>
2018-09-15 14:00:04 -0400

    Simple program to print default text

--- PulseEMBCOMBill/Program.cs
+++ PulseEMBCOMBill/Program.cs
@@ -12,6 +12,25 @@ namespace PulseEMBCOMBill
     {
         static void Main(string[] args)
         {
+            IApplication PulseID = new Pulse.Application();
+  
+            IEmbDesign Design = PulseID.OpenDesign("C:\\Temp\\Emoji.PXF", FileTypes.ftAuto, OpenTypes.otOutlines, "Tajima");
+            IElements TemplateElements = Design.Elements;
+          
+            foreach (IElement Element in TemplateElements) 
+            {
+                //Go through each element in a template
+
+                Console.WriteLine ("Element Name: {0:G}",Element.Name);
+                for (int i = 0; i < Element.NumText; i++)
+                {
+                    // Go through each text element and print the default text
+
+                    Console.WriteLine("Default Text: {0:G}", Element.Texts.GetValue(i));
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
